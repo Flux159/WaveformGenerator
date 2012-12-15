65ec1249f8adcc31b9b5b58e52fe7e69d580154e
Suyog Sonwalkar <Flux159@gmail.com>
2012-12-15 15:57:28 -0500

    Structure in place for creating png image and processing wav file to create waveform data.

--- src/waveformgenerator.c
+++ src/waveformgenerator.c
@@ -7,17 +7,20 @@
 * 
 * 12-14-2012
 *
-* Usage:
+* Usage: >> waveformgenerator <input-wav-file> <output-png-file>
 *
 */
 
+//File input and output & memory allocation (malloc and free)
 #include <stdio.h>
 #include <stdlib.h>
 
+//Utility classes for argument parsing and string manipulation
 #include <unistd.h>
 #include <string.h>
 #include <stdarg.h>
 
+//libpng for creating png images
 #define PNG_DEBUG 3
 #include <png.h>
 
@@ -64,7 +67,11 @@ void dealloc_wavdata(void* wavdata){
 	free(wavdata);
 }
 
-void create_png_image(){
+void* init_waveform_and_process_wavdata(void* wavdata){
+	return NULL;
+}
+
+void create_png_image(FILE* fp, void* waveformdata){
 	//TODO: Implement create_png_image
 	// printf("In create_png_image\n");
 }
@@ -78,19 +85,25 @@ int main(int argc, char *argv[]){
 	}
 	
 	// Read from the wav file (command line argument 1)
-	FILE* fp = fopen(argv[1], "rb");
+	FILE* wavfile = fopen(argv[1], "rb");
 	
 	//Read wav file header here
-	wavheader* header = init_and_read_wavheader(fp);
+	wavheader* header = init_and_read_wavheader(wavfile);
 	
 	//Read wav file data here
-	void* wavdata = init_and_read_wavdata(fp, header->subChunk2Size);
+	void* wavdata = init_and_read_wavdata(wavfile, header->subChunk2Size);
 	
 	//Close the file pointer for the wav file input
-	fclose(fp);
+	fclose(wavfile);
+	
+	void* waveformdata = init_waveform_and_process_wavdata(wavdata);
+	
+	FILE* pngfile = fopen(argv[2], "wb");
 	
 	//Create png image here with waveform
-	create_png_image();
+	create_png_image(pngfile, waveformdata);
+	
+	fclose(pngfile);
 		
 	dealloc_wavheader(header);
 	dealloc_wavdata(wavdata);
